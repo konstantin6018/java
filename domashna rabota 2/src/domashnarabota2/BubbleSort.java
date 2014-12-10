4c1fe1966fc3338f4c1b56d2c56bc429594857cb
konstantin6018 <papazov.konstantin@gmail.com>
2014-12-10 21:36:13 +0200

    Commit

---
+++ domashna rabota 2/src/domashnarabota2/BubbleSort.java
@@ -0,0 +1,35 @@
+package domashnarabota2;
+import java.util.*;
+public class BubbleSort {
+
+	public static void main(String[] args) {
+			  
+			    int i,d, temp;
+			    Scanner in = new Scanner(System.in);
+			    int arr[] = new int[5];
+			 
+			    System.out.println("Vuvedi 5 celochisleni chisla:");
+			 
+			    for (i = 0; i < 5; i++) 
+			      arr[i] = in.nextInt();
+			 
+			    for (i = 0; i < 4; i++) {
+			      for (d = 0; d < 5 - i - 1; d++) {
+			        if (arr[d] > arr[d+1]) 
+			        {
+			          temp       = arr[d];
+			          arr[d]   = arr[d+1];
+			          arr[d+1] = temp;
+			        }
+			      }
+			    }
+			 
+			    System.out.println("Sled sortiraneto:");
+			 
+			    for (i = 0; i < 5; i++) 
+			      System.out.println(arr[i]);
+			  }
+		
+	}
+
+
